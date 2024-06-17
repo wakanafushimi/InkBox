a6e29fe9d0e325c92ab8c7a46b92f410b874800e
moemi <86924333+mirandakahori@users.noreply.github.com>
2024-06-17 14:14:36 +0900

    Update host.js

--- front-end/connection/host.js
+++ front-end/connection/host.js
@@ -2,6 +2,18 @@
 // const token =
 //   "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MTgzNTY4NDgsInVzZXJJZCI6MX0.BzEz2MO3juAiNO-DCUCfuLPbh5IwK1VLctQmfLbUZjE";
 // module.exports = { host, token };
-export const host = "http://52.72.215.5:8080"; // あるいは適切なホスト情報
-export const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MTgzNTY4NDgsInVzZXJJZCI6MX0.BzEz2MO3juAiNO-DCUCfuLPbh5IwK1VLctQmfLbUZjE"; // 必要に応じてトークンなどを追加
+// export const host = "http://52.72.215.5:8080"; // あるいは適切なホスト情報
+// export const token =
+//   "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MTgzNTY4NDgsInVzZXJJZCI6MX0.BzEz2MO3juAiNO-DCUCfuLPbh5IwK1VLctQmfLbUZjE"; // 必要に応じてトークンなどを追加
+
+
+
+//Node.jsバージョン: Node.js v20.14.0のコードです↓ （by もえみ）
+module.exports = {
+  host: 'http://52.72.215.5:8080',
+  token: 'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MTgzNTY4NDgsInVzZXJJZCI6MX0.BzEz2MO3juAiNO-DCUCfuLPbh5IwK1VLctQmfLbUZjE',
+};
+
+
+
+
